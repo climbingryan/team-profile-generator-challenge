b1338f9a3a9c26be87adde4443c7c36a994d06bf
shaggy <simmons5family@gmail.com>
2020-12-01 18:36:20 -0600

    passing test for getId method

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,12 +4,18 @@ test('creates a Employee object', () => {
     const employee = new Employee('kevkev');
 
     expect(employee.name).toBe('kevkev');
-    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.id).toEqual(expect.any(String));
     expect(employee.email).toBe('');
 });
 
 test("get employee's name", () => {
     const employee = new Employee('kevkev');
-    
+
     expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('get employee id', () => {
+    const employee = new Employee('kevkev');
+
+    expect(employee.getId()).toEqual(expect.any(String));
 });
