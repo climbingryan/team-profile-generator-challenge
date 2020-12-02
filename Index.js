871a01311c63c61bc7025e6dcdf34828938055d3
shaggy <simmons5family@gmail.com>
2020-12-02 15:10:25 -0600

    inquirer prompts for Manager

---
+++ Index.js
@@ -0,0 +1,63 @@
+const inquirer = require('inquirer');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+function Start() {
+    inquirer
+        .prompt([
+            {
+                type: 'text',
+                name: 'teamManagerName',
+                message: 'What is the name the team manager?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'number',
+                name: 'employeeId',
+                message: 'What is your employee id?',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Please enter a number.');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'text',
+                name: 'ManagerEmailAddress',
+                message: 'What is your email address',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        console.log('Please enter an email');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'number',
+                name: 'officeNumber',
+                message: 'What is your office number?',
+                validate: input => {
+                    if (input) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        ]);
+}
+
+Start();
